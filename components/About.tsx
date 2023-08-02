48f9cfbde8e692480937568e2ecd156e7848a17e
Daniel Famiyeh <daniel@podaris.com>
2023-08-02 07:23:17 +0100

    Make projects section responsive

---
+++ components/About.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { fonts } from "@/utils/fonts";
+
+export default function About() {
+  return (
+    <>
+      <div className="flex flex-col items-center lg:flex-row justify-between mb-36">
+        <div className="lg:w-4/12">
+          <Image
+            className="rounded-full mb-16 lg:mb-0"
+            alt="Picture of Daniel Famiyeh"
+            src="/profile.jpg"
+            width={400}
+            height={400}
+          />
+        </div>
+        <div className="lg:w-2/12"></div>
+        <div className="lg:w-6/12 text-center lg:text-left">
+          <h1 className={`${fonts.title.className} text-[#f0ebd8] text-6xl`}>
+            Welcome!
+          </h1>
+          <p className="text-white mt-4">
+            Meet John Hawkins, the virtuoso behind these sleek web creations. An
+            expert in minimalism, John combines form and function to weave
+            digital dreams into reality.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,23 +1,10 @@
-import { fonts } from "@/utils/fonts";
+import About from "./About";
 import Projects from "./Projects";
 
 export default function Main() {
   return (
     <div className="max-w-[1000px] mx-auto pt-24">
-      <div className="flex justify-between mb-36">
-        <div className="w-4/12">Image</div>
-        <div className="w-2/12"></div>
-        <div className="w-6/12">
-          <h1 className={`${fonts.title.className} text-[#f0ebd8] text-6xl`}>
-            Welcome!
-          </h1>
-          <p className="text-white mt-4">
-            Meet John Hawkins, the virtuoso behind these sleek web creations. An
-            expert in minimalism, John combines form and function to weave
-            digital dreams into reality.
-          </p>
-        </div>
-      </div>
+      <About />
       <Projects />
     </div>
   );
