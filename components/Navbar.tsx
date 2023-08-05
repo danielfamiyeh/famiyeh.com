8bb14d05786f800b3acb15fe2273292395d78efc
Daniel Famiyeh <daniel@podaris.com>
2023-08-05 17:49:57 +0100

    Add hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 const pages = [
   { title: "About" },
   { title: "Experience" },
@@ -8,8 +10,10 @@ const pages = [
 ];
 
 export default function Navbar({ isHeroTitleVisible }: NavbarProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="flex justify-between items-center py-4 w-full">
+    <nav className="flex justify-between items-center py-4 lg:py-px w-full">
       <div className="flex gap-1">
         <p className="m-0">Daniel</p>
         <b
@@ -20,16 +24,31 @@ export default function Navbar({ isHeroTitleVisible }: NavbarProps) {
           Famiyeh
         </b>
       </div>
-      <ul className="flex flex-col lg:flex-row text-center">
-        {pages.map(({ title }) => (
-          <li
-            key={`nav-link-${title}`}
-            className="mr-4 select-none cursor-pointer"
-          >
-            {title}
-          </li>
-        ))}
-      </ul>
+      <div>
+        <div
+          className="burger lg:hidden flex flex-col gap-1 cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div className="h-0.5 w-5 bg-black" />
+          <div className="h-0.5 w-5 bg-black" />
+          <div className="h-0.5 w-5 bg-black" />
+        </div>
+        <ul
+          className={`flex flex-col fixed right-0 text-right mt-5 bg-[#eeeeee] w-full overflow-hidden ${
+            isOpen ? "max-h-[9999px]" : "max-h-0"
+          } transition-all duration-700 ease-in-out lg:max-h-[9999px] lg:mt-0 lg:flex-row lg:relative`}
+        >
+          {pages.map(({ title }) => (
+            <li
+              key={`nav-link-${title}`}
+              className="mr-4 p-2 text-center select-none cursor-pointer border-t-2 lg:border-t-0"
+              onClick={() => setIsOpen(false)}
+            >
+              {title}
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 }
