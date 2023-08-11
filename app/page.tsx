727a72aa9a17897e79c5008d6c7bbcb37feab1f2
Daniel Famiyeh <daniel@podaris.com>
2023-08-11 16:49:55 +0100

    Change main bg color

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
   return (
     <main>
-      <div className="px-4 lg:px-48 fixed w-full bg-[#F0EBD8] z-50">
+      <div className="px-4 lg:px-48 fixed w-full bg-[#D4D4D4] z-50">
         <Navbar
           aboutRef={aboutRef}
           contactRef={contactRef}
@@ -31,7 +31,7 @@ export default function Home() {
           isHeroTitleVisible={isHeroTitleVisible}
         />
       </div>
-      <div className="px-4 lg:px-48 min-h-screen pt-32 lg:pt-16 pb-16 bg-[#F0EBD8]">
+      <div className="px-4 lg:px-48 min-h-screen pt-32 lg:pt-16 pb-16 bg-[#D4D4D4]">
         <Hero heroTitleRef={heroTitleRef} />
       </div>
       <div className="min-h-screen w-full bg-[#1D2D44] px-4 lg:px-48 pb-16">
