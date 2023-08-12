f8141c0d5b866078364c5f69f9612ba10087d055
Daniel Famiyeh <daniel@podaris.com>
2023-08-12 13:35:48 +0100

    Turn skills on project archive into pill buttons

--- app/projects/archive/page.tsx
+++ app/projects/archive/page.tsx
@@ -2,12 +2,13 @@
 
 import { useEffect, useState } from "react";
 
+import Link from "@/components/Link";
+
 import { useDatabaseData } from "@/utils/hooks/useDatabaseData";
 import { getProjectsAction } from "@/app/_actions";
 import { Project } from "@/models/Project";
 import { linkIcons } from "@/utils/links";
 import { fonts } from "@/utils/fonts";
-import Link from "@/components/Link";
 
 export default function ProjectArchive() {
   const projects = useDatabaseData<Project>({
@@ -37,11 +38,11 @@ export default function ProjectArchive() {
 
       <hr className="mb-8" />
 
-      <table className="table-auto bg-[#253956] self-center text-[#eee] rounded-lg">
+      <table className="table-auto bg-[#253956] self-center text-[#eee] rounded-lg text-sm">
         <thead className="p-4">
           <tr>
             <td className="py-4 px-4">Title</td>
-            <td className="px-4">Subtitle</td>
+            <td className="px-4">Description</td>
             <td className="px-4">Skills</td>
             <td className="px-4">Links</td>
           </tr>
@@ -53,10 +54,18 @@ export default function ProjectArchive() {
 
             return (
               <tr key={`project-${id}`} className="border-t border-[#4a6182]">
-                <td className="px-4 py-2">{project.title}</td>
+                <td className="px-4 py-2">
+                  <b>{project.title}</b>
+                </td>
                 <td className="px-4 py-2">{project.subtitle}</td>
-                <td className="px-4 py-2">{project.skills.join(", ")}</td>
-                <td className="px-4 py-2 flex flex-wrap items-center justify-center">
+                <td className="px-4 py-2">
+                  {project.skills.map((skill) => (
+                    <div className="bg-slate-500 px-4 py-px mr-2 rounded-lg inline-block">
+                      {skill}
+                    </div>
+                  ))}
+                </td>
+                <td className="px-4 py-4 text-center">
                   {project.links.map((link) => {
                     const Icon =
                       linkIcons[
